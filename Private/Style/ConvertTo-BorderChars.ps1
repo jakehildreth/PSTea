1929575403b9616a08463ea786faf45d1ab25819
Jake Hildreth <93942157+jakehildreth@users.noreply.github.com>
2026-04-23 10:29:35 -0400

    fix(compat): replace literal Unicode box-drawing chars with [char]0x... codepoints
    - ConvertTo-BorderChars.ps1: Normal/Rounded/Thick/Double borders now use
      [char]0x... instead of literal Unicode glyphs
    - PS5.1/conhost reads files as Windows-1252 by default, mangling UTF-8
      multibyte chars at parse time; codepoints are ASCII-safe

--- Private/Style/ConvertTo-BorderChars.ps1
+++ Private/Style/ConvertTo-BorderChars.ps1
@@ -6,11 +6,11 @@ function ConvertTo-BorderChars {
     )
 
     $borderMap = @{
-        'None'    = [PSCustomObject]@{ TL = '';  T = '';  TR = '';  L = '';  R = '';  BL = '';  B = '';  BR = '' }
-        'Normal'  = [PSCustomObject]@{ TL = '┌'; T = '─'; TR = '┐'; L = '│'; R = '│'; BL = '└'; B = '─'; BR = '┘' }
-        'Rounded' = [PSCustomObject]@{ TL = '╭'; T = '─'; TR = '╮'; L = '│'; R = '│'; BL = '╰'; B = '─'; BR = '╯' }
-        'Thick'   = [PSCustomObject]@{ TL = '┏'; T = '━'; TR = '┓'; L = '┃'; R = '┃'; BL = '┗'; B = '━'; BR = '┛' }
-        'Double'  = [PSCustomObject]@{ TL = '╔'; T = '═'; TR = '╗'; L = '║'; R = '║'; BL = '╚'; B = '═'; BR = '╝' }
+        'None'    = [PSCustomObject]@{ TL = '';                  T = '';                  TR = '';                  L = '';                  R = '';                  BL = '';                  B = '';                  BR = '' }
+        'Normal'  = [PSCustomObject]@{ TL = [char]0x250C;        T = [char]0x2500;        TR = [char]0x2510;        L = [char]0x2502;        R = [char]0x2502;        BL = [char]0x2514;        B = [char]0x2500;        BR = [char]0x2518 }
+        'Rounded' = [PSCustomObject]@{ TL = [char]0x256D;        T = [char]0x2500;        TR = [char]0x256E;        L = [char]0x2502;        R = [char]0x2502;        BL = [char]0x2570;        B = [char]0x2500;        BR = [char]0x256F }
+        'Thick'   = [PSCustomObject]@{ TL = [char]0x250F;        T = [char]0x2501;        TR = [char]0x2513;        L = [char]0x2503;        R = [char]0x2503;        BL = [char]0x2517;        B = [char]0x2501;        BR = [char]0x251B }
+        'Double'  = [PSCustomObject]@{ TL = [char]0x2554;        T = [char]0x2550;        TR = [char]0x2557;        L = [char]0x2551;        R = [char]0x2551;        BL = [char]0x255A;        B = [char]0x2550;        BR = [char]0x255D }
     }
 
     if ($borderMap.ContainsKey($Style)) {
